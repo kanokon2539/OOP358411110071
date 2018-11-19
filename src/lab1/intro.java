35d8a09059dba58c04ec87e35e50236a3efc9a7e
tukta <kanokonkaewthaworn11@gmail.com>
2018-11-19 16:35:57 +0700

    Lab 1 commit

--- src/lab1/intro.java
+++ src/lab1/intro.java
@@ -1,4 +1,23 @@
 package lab1;
 
 public class intro {
-}
+    //to
+    public static void main(String[] args) {
+     System.out.println("kanokon kaewthaworn");
+     System.out.println("kanokonkaewthaworn11@gmail.com");
+     System.out.println("MT");
+     System.out.println("RMUTSV");
+
+
+
+
+
+
+    }//main
+
+
+
+
+
+
+}//class
