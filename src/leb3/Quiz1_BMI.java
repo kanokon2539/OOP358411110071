96dc3f2fb42edf8f439ff26a4ef9762b66bf8e28
tukta <kanokonkaewthaworn11@gmail.com>
2018-12-15 17:18:37 +0700

    Lab 2:

---
+++ src/leb3/Quiz1_BMI.java
@@ -0,0 +1,36 @@
+package leb3;
+
+import javax.xml.soap.Text;
+import java.util.Scanner;
+
+public class Quiz1_BMI {
+
+    public static void main(String[] args) {
+        double w,h;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter your weight: ");
+        w = sc.nextDouble();
+        System.out.print("Enter your weight (m): ");
+        h = sc.nextDouble();
+
+        //calculate BMI
+        double bmi = w / (h * h);
+        System.out.println("BMI ="+bmi);
+        //Text condition
+          if (bmi <18.50)
+              System.out.println("น้ำหนักน้อย/ผอม");
+        else if ((bmi >=18.50)&&(bmi <=22.90))
+              System.out.println("ปกติ/สุขภาพ");
+          else if ((bmi >=23)&&(bmi <=22.90))
+              System.out.println("ท้วม/โรคอ้วนระดับที่ 1");
+          else if ((bmi >=25)&&(bmi <=29.90))
+              System.out.println("อ้วน/โรคอ้วนระดับที่ 2");
+          else
+              System.out.println("อ้วน/โรคอ้วนระดับที่ 3");
+
+
+
+    }//main
+
+}//class
