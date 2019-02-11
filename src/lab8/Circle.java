628d486a4c0f3d3901fa1ace77bb70a433b27416
tukta <kanokonkaewthaworn11@gmail.com>
2019-02-11 15:54:26 +0700

    Lab 8 : OOP-Inherit

---
+++ src/lab8/Circle.java
@@ -0,0 +1,21 @@
+package lab8;
+
+public class Circle extends GraphicObject{
+    private  double r;
+    final double PI = 3.14;
+
+    @Override
+    void draw() {
+        System.out.println("Drawing Circle...");
+
+
+    }
+
+    @Override
+    double findArea() {
+        return PI*(r*r);
+    }
+    public Circle (double r){
+
+    }
+}//class
